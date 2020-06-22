6b3d7ce76743147128ed3bfe10349d82dc1c96f0
devWRM <mcwen@comcast.net>
2020-06-21 22:31:25 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        list.select do |match|
+            match.split(//).sort == @word.split(//).sort
+        end
+    end
+
+end
+
+
+
