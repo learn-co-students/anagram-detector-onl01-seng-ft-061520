9823d8125ac2184226b965c6b37cb3fa493f3c7a
kak79 <kohelka@gmail.com>
2020-06-25 17:36:20 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+class Anagrams
+    attr_accessor :word
+
+    @@WORDS = []
+
+    def initialize(word)
+        @word = word
+    end
+
+
+    def words_to_letters
+        @@WORDS.chars = []
+    end
+
+    
+
+
+
+
+end
