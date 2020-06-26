da7a9ccbf4a09ca89eeff94eb90654bc36cbabdc
kak79 <kohelka@gmail.com>
2020-06-26 07:33:31 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,23 +2,12 @@ require 'pry'
 class Anagram
     attr_accessor :word
 
-    # @@WORDS = []
-
     def initialize(word)
         @word = word
     end
 
-
-    # def words_to_letters
-    #     @@WORDS.chars = []
-    # end
-
-    # def shuffle_letters
-    #     words_to_letters.sort_by
-    # end
-
-    def match(lwords)
-        lwords.each do | lword | 
+    def match(lists)
+        lists.keep_if { |word| word.split("").sort == @word.split("").sort }
     end
 
 
