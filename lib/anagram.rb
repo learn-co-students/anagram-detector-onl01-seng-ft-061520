07b3ec4f49e1a3e35c32e47fca36ba6737d142f8
kak79 <kohelka@gmail.com>
2020-06-25 19:55:14 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,25 @@
 require 'pry'
-class Anagrams
+class Anagram
     attr_accessor :word
 
-    @@WORDS = []
+    # @@WORDS = []
 
     def initialize(word)
         @word = word
     end
 
 
-    def words_to_letters
-        @@WORDS.chars = []
+    # def words_to_letters
+    #     @@WORDS.chars = []
+    # end
+
+    # def shuffle_letters
+    #     words_to_letters.sort_by
+    # end
+
+    def match(lwords)
+        lwords.each do | lword | 
     end
-
-    
-
-
 
 
 end
